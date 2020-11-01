8913d15b9a1b6cb5265b2f78f3a4c4bfe1f36095
chenhongsheng <707479149@qq.com>
2020-11-02 01:19:07 +0800

    test

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export default {
     api_host_dev: "http://localhost:10004/v1/admin",
-    api_host_prod:"ttps://mp-test.chargersniff.com/admin",
+    api_host_prod:"https://mp-test.chargersniff.com/admin",
 }
